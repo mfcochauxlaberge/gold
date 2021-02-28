0bac51896dda031436db2aaa9273a0914a168208
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-02-28 13:48:45 -0500

    implement tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mfcochauxlaberge/gold
 
 go 1.15
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+)
--- gold_test.go
+++ gold_test.go
@@ -1,6 +1,7 @@
 package gold_test
 
 import (
+	"io/ioutil"
 	"testing"
 
 	. "github.com/mfcochauxlaberge/gold"
@@ -12,5 +13,135 @@ func TestNewRunner(t *testing.T) {
 	assert := assert.New(t)
 
 	runner := NewRunner("some/path")
+	assert.Equal(false, runner.Update)
 	assert.Equal("some/path", runner.Directory)
+	assert.Len(runner.Filters, 0)
+}
+
+func TestRunnerPrepare(t *testing.T) {
+	assert := assert.New(t)
+
+	dir, err := ioutil.TempDir("", "")
+	assert.NoError(err)
+
+	// Put a file in the directory.
+	_, err = ioutil.TempFile(dir, "")
+	assert.NoError(err)
+
+	// Make sure it exists.
+	files, err := ioutil.ReadDir(dir)
+	assert.NoError(err)
+	assert.Equal(1, len(files))
+
+	// Create runner and prepare it.
+	runner := NewRunner(dir)
+
+	// Nothing happens here because
+	// update mode is disabled.
+	err = runner.Prepare()
+	assert.NoError(err)
+
+	files, err = ioutil.ReadDir(dir)
+	assert.NoError(err)
+	assert.Equal(1, len(files))
+
+	// The directory gets deleted and
+	// recreated in Prepare, so it
+	// should be empty after the call.
+	runner.Update = true
+
+	err = runner.Prepare()
+	assert.NoError(err)
+
+	files, err = ioutil.ReadDir(dir)
+	assert.NoError(err)
+	assert.Equal(0, len(files))
+}
+
+func TestRunnerTest(t *testing.T) {
+	assert := assert.New(t)
+
+	dir, err := ioutil.TempDir("", "")
+	assert.NoError(err)
+
+	// Create runner and prepare it.
+	runner := NewRunner(dir)
+	runner.Update = true
+
+	err = runner.Prepare()
+	assert.NoError(err)
+
+	// With update mode on, the following calls will
+	// create files.
+	err = runner.Test("test1", []byte("This is a test."))
+	assert.NoError(err)
+
+	err = runner.Test("test2", []byte("This is another test."))
+	assert.NoError(err)
+
+	// The update mode is turned off. The following should
+	// not return an error since the files have already
+	// been created.
+	runner.Update = false
+
+	err = runner.Test("test1", []byte("This is a test."))
+	assert.NoError(err)
+
+	err = runner.Test("test2", []byte("This is another test."))
+	assert.NoError(err)
+}
+
+func TestRunnerWithFilter(t *testing.T) {
+	assert := assert.New(t)
+
+	dir, err := ioutil.TempDir("", "")
+	assert.NoError(err)
+
+	// Create runner and prepare it.
+	runner := NewRunner(dir)
+	runner.Update = true
+
+	err = runner.Prepare()
+	assert.NoError(err)
+
+	runner.Filters = append(runner.Filters, func(src []byte) []byte {
+		return []byte("The output was altered.")
+	})
+
+	// Create file.
+	err = runner.Test("test1", []byte("The output was not altered."))
+	assert.NoError(err)
+
+	// Turn off update mode and check file.
+	runner.Update = false
+
+	err = runner.Test("test1", []byte("The output was altered."))
+	assert.NoError(err)
+}
+
+func TestRunnerTestComparisonError(t *testing.T) {
+	assert := assert.New(t)
+
+	dir, err := ioutil.TempDir("", "")
+	assert.NoError(err)
+
+	// Create runner and prepare it.
+	runner := NewRunner(dir)
+	runner.Update = true
+	err = runner.Prepare()
+	assert.NoError(err)
+
+	// Create file.
+	err = runner.Test("test1", []byte("This is a test."))
+	assert.NoError(err)
+
+	// Turn off update mode and check file.
+	runner.Update = false
+
+	err = runner.Test("test1", []byte("The content is different."))
+	assert.Error(err)
+
+	cErr, ok := err.(ComparisonError)
+	assert.True(ok)
+	assert.Equal("output and file are different", cErr.Error())
 }
